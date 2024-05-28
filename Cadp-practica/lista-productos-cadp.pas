fff1f888b232f83b648e3595f9077a4bf0441af8
tobias gonzalez arriola <100397465+crtobias@users.noreply.github.com>
2024-05-27 22:54:12 -0300

    Update lista-productos-cadp.pas

--- Cadp-practica/lista-productos-cadp.pas
+++ Cadp-practica/lista-productos-cadp.pas
@@ -2,18 +2,15 @@ program test;
 
 type
 
-//2000 productos con name , marca ,rubos(1..15)
-
 prod = record
     nombre:string;
     marca:string
     rubro: 1..15;
 end;
-
+//array principal con productos
 productos : array[1..2000] of prod;
 
-
-fec = record
+fec = record //fecha tipo
     dia: 1..31;
     mes: 1..13;
 end;
@@ -21,24 +18,60 @@ end;
 lista : ^nodo;  //lista de productos
 listaPedidos : ^nodoPedidos; // lista de pedidos
 
-nodo = record 
+nodoPedidos = record  //nodo de pedidos
     dato: pedido;
     sig: listaPedidos;
 end;
 
 productoPedir = record
-    cant:integer;
-    pr:prod;
+    cant:integer; //cantidad de compra
+    pr:integer; //int del prducto productos[2]
 end;
 
-nodo = record
+nodo = record //nodo de productos del pedido
     dato: productoPedir;
     sig: lista;
 end;
 
-pedido = record
+pedido = record //pedido
     fecha: fec;
     num: integer;
     productos: lista;
 end;
 
+conteoArray : array[1..15] of integer;
+
+
+//lpedidos lista de pedidos cargadas //nodo pedido
+//cArray para contar 1..15
+//Aproductos lista de productos iniciales 1..2000
+
+procedure consginaA(Lpedidos:listaPedidos;var cArray:conteoArray; Aproductos:productos);
+begin
+
+    while (Lpedidos <> nil) do begin
+        while (Lpedidos^.pedido.productos <> nil) do begin
+            //posicion del producto actual
+            //Aproductos[Lpedidos^.pedido.productos^.dato.pr].rubro
+
+            //cantidad de producto actual
+            // Lpedidos^.pedido.productos^.dato.cant 
+            
+            //en el array en el que cuento los 15 rubros sumo la cantidad de productos 
+            // que hay en cada rubro
+
+            cArray[Aproductos[Lpedidos^.pedido.productos^.dato.pr].rubro] :=  cArray[Aproductos[Lpedidos^.pedido.productos^.dato.pr].rubro] + Lpedidos^.pedido.productos^.dato.cant 
+            //cArray[posiciondel producto.rubro] := cArray[posiciondel producto.rubro] + cantidad de pedidos
+            
+
+            Lpedidos^.pedido.productos := Lpedidos^.pedido.productos^.sig;            
+        end;
+        Lpedidos := Lpedidos^.sig;
+    end;
+
+    //imprimo el array
+    for i:=1 to 15 do begin
+        writeln('cantidad de producto en el rubro ', i ,'  son  ', cArray[i])
+    end;
+
+end;
